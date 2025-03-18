bd1aabf976dee45614191a7ff49442d900f4347b
Loic1363 <loic.hardy@neurogreen.be>
2025-03-18 16:41:03 +0100

    libc

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,7 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=/home/loic/ASSIGNMENT1/finder-app
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-
+LIBC_PATH=/home/loic/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
 
 echo "Vérification du compilateur croisé : $CROSS_COMPILE"
 which aarch64-none-linux-gnu-gcc
